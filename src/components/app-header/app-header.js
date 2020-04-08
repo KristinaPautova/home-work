2784a71976c8372ac990529b83252d10ce869c55
Christina <christie.pautova@gmail.com>
2020-04-08 13:32:26 +0600

    HOmeWork

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css'
+
+const AppHeader = () => {
+    return (
+        <div className="app-header d-flex">
+            <h1>To do list</h1>
+            <h2>Kristina Pautova</h2>
+        </div>
+    )
+}
+
+export default AppHeader;
