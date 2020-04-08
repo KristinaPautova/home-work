4bb6df36fa87ca24e38568fa35faf75268aae8ac
Christina <christie.pautova@gmail.com>
2020-04-08 21:48:22 +0600

    commit2

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,24 +1,70 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import FormLabel from '@material-ui/core/FormLabel';
+import FormControl from '@material-ui/core/FormControl';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Checkbox from '@material-ui/core/Checkbox';
+import AppHeader from '../app-header/app-header'
 
-import AppHeader from '../app-header';
-import PostList from '../post-list';
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex'
+  },
+  formControl: {
+    margin: theme.spacing(3),
+  },
+}));
 
+export default function CheckboxesGroup() {
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    gilad: true,
+    jason: false,
+    antoine: false,
+    dog: false,
+    cook: false
+  });
 
-const App = () => {
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
 
-    const data = [
-        {label: 'Сходить в магазин', important: true, id: 1},
-        {label: 'Изучать React', important: false, id: 2},
-        {label: 'Навестить к бабушке', important: false, id: 3},
-        {label: 'Обнять Жужу', important: false, id: 3},
-        {label: 'Научиться готовить', important: false, id: 3}
-    ]
-    return (
-       <div>
-            <AppHeader/>
-            <PostList posts={data}/>
-       </div>
-    )
+  const { gilad, jason, antoine, dog, cook } = state;
+  const error = [gilad, jason, antoine, dog, cook].filter((v) => v).length !== 2;
+
+  return (
+    <div className={classes.root}>
+      <FormControl component="fieldset" className={classes.formControl}>
+        <AppHeader/>
+        <FormGroup>
+          <FormControlLabel
+            control={<Checkbox checked={gilad} onChange={handleChange} name="gilad" />}
+            label="Сходить в магазин"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={jason} onChange={handleChange} name="jason" />}
+            label="Изучать React"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={antoine} onChange={handleChange} name="antoine" />}
+            label="Навестить бабушку"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={dog} onChange={handleChange} name="dog" />}
+            label="Обнять Жужу"
+          />
+          <FormControlLabel
+            control={<Checkbox checked={cook} onChange={handleChange} name="cook" />}
+            label="Научиться готовить"
+          />          
+        </FormGroup>
+        <FormHelperText>the end</FormHelperText>
+      </FormControl>
+      <FormControl required error={error} component="fieldset" className={classes.formControl}>
+       
+      </FormControl>
+    </div>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './components/app/';
 
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 
